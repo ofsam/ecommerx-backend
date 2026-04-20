70f757b8c654a68594dcb835deba9575c381e9f6
ofsam123 <ad12@gmail.com>
2026-04-20 10:05:59 +0000

    redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,8 @@
-const Redis = require("ioredis");
+const { Redis } = require("@upstash/redis");
 
-const redis = new Redis(process.env.REDIS_URL, {
-  maxRetriesPerRequest: null,
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
 module.exports = redis;
